f5a3659a80eeeaaf86a449823045113691532873
Sornram Saetang <waterfishX@outlook.com>
2020-05-13 12:31:27 +0700

    Add drag and drop funcitonalities

--- script.js
+++ script.js
@@ -20,6 +20,39 @@ const richestPeople = [
 
   createList();
 
+  function dragStart() {
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+  }
+  
+  function dragEnter(e) {
+    console.log('in');
+    this.closest('li').classList.add('over');
+  }
+  
+  function dragLeave() {
+    console.log('out');
+    this.closest('li').classList.remove('over');
+  }
+  
+  function dragOver(e) {
+    e.preventDefault();
+  }
+  
+  function dragDrop() {
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+  }
+
+  function swapItems(fromIndex, toIndex) {
+      const itemOne = listItems[fromIndex].querySelector('.draggable');
+      const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+      listItems[fromIndex].appendChild(itemTwo);
+      listItems[toIndex].appendChild(itemOne);
+  }
+
   function createList() {
       [...richestPeople]
         .map(a => ({ value: a, sort: Math.random()}))
@@ -41,5 +74,26 @@ const richestPeople = [
             listItems.push(listItem);
 
             draggable_list.appendChild(listItem);
-        })
+        });
+
+        addEventListeners();
   }
+
+  function addEventListeners() {
+      const draggables = document.querySelectorAll('.draggable');
+      const dragListItems = document.querySelectorAll('.draggable-list li');
+
+      draggables.forEach(draggable => {
+          draggable.addEventListener('dragstart', dragStart);
+      });
+
+      dragListItems.forEach(item => {
+          item.addEventListener('dragover', dragOver);
+          item.addEventListener('dragenter', dragEnter);
+          item.addEventListener('dragleave', dragLeave);
+          item.addEventListener('drop', dragDrop);
+
+      })
+  }
+
+  
